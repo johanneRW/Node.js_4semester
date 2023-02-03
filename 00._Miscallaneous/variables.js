cf6043a53c9baa87220e4f61e7dd9fd2d6215c8c
johanne <joha786s@stud.kea.dk>
2023-02-03 10:35:34 +0100

    Created a ariables file

---
+++ 00._Miscallaneous/variables.js
@@ -0,0 +1,13 @@
+
+//Const -Rule 1: Have to assigne a value when declaring
+//const me
+
+//key:value === key-value pairs
+const me ={name:string
+};
+
+// Const Rule 2: can't reassign to contstant
+//me={};
+me.name="Johanne";
+
+console.log(me);

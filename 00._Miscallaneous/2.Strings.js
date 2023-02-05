942fd6f20c2d5678870bb1cd78ee3376e1b912a4
johanne <joha786s@stud.kea.dk>
2023-02-05 21:15:21 +0100

    solved most  of 2.string, still an error

--- 00._Miscallaneous/2.Strings.js
+++ 00._Miscallaneous/2.Strings.js
@@ -6,7 +6,9 @@ const numberTwo = "2.30";
 
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
+const result=Number(numberOne)+Number(numberTwo)
 
+console.log(result)
 
 // --------------------------------------
 
@@ -17,6 +19,10 @@ const numberTwo = "2.30";
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
+const resultOne=Number(anotherNumberOne)+Number(anotherNumberTwo)
+const resultDeci=resultOne.toFixed(2)
+
+console.log(resultDeci)
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -25,6 +31,9 @@ const one = 10;
 const two = 45;
 const three = 98;
 
+const avgNumber=(one + two + three)/3
+
+console.log(avgNumber.toFixed(5))
 // Show in the console the avg. with 5 decimals
 
 
@@ -37,8 +46,9 @@ const three = 98;
 const letters = "abc";
 // Get me the character "c"
 
+const char=letters.charAt(2)
 
-
+console.log(char)
 
 // --------------------------------------
 // Exercise 7 - Replace
@@ -46,8 +56,8 @@ const letters = "abc";
 const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
-
-
+const factWhitUpperJ=fact.replace("j","J")
+console.log(factWhitUpperJ)
 // --------------------------------------
 
 

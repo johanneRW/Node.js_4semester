9d4e4b6368950a501f2092e18ee55d077cd1ce76
johanne <joha786s@stud.kea.dk>
2023-02-15 17:19:53 +0100

    birds API: GET methods

---
+++ 01._Birds_api/bird_app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let listOfBirds = [
+   {
+      'id': 1,
+      'name': 'Bent',
+      'species': 'Blackbird',
+      'age': 4
+   },
+   {
+      'id': 2,
+      'name': 'Mette',
+      'species': 'Blue tit',
+      'age': 7
+   },
+]
+
+app.get('/', (req, res) => {
+   res.send('Welcome to the birds-app!')
+})
+
+
+app.get('/birds/', (req, res) => {
+   res.send(listOfBirds)
+})
+
+app.get('/birds/:id', (req, res) => {
+
+   const matchingId = (bird) => {
+      return bird.id === Number(req.params.id)
+   }
+
+   const match = listOfBirds.find(matchingId)
+
+   if (match !== undefined) {
+      res.send(match)
+   } else {
+      res.sendStatus(404)
+   }
+
+})
+
+
+app.listen(8080)

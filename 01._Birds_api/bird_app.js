16174057789749206bbdd4d895b74b621d52651a
johanne <joha786s@stud.kea.dk>
2023-02-22 20:22:48 +0100

    made POST, PUT, PATCH and DELETE endpoints

--- 01._Birds_api/bird_app.js
+++ 01._Birds_api/bird_app.js
@@ -3,6 +3,10 @@ const app = express()
 
 app.use(express.json())
 
+app.listen(8080, () => {
+   console.log("Server is running on port", 8080)
+})
+
 let listOfBirds = [
    {
       'id': 1,
@@ -18,30 +22,96 @@ let listOfBirds = [
    },
 ]
 
+const makeMatchingId = (id) => {
+   return (bird) => {
+      return bird.id === Number(id)
+   }
+}
+
 app.get('/', (req, res) => {
    res.send('Welcome to the birds-app!')
 })
 
 app.get('/birds/', (req, res) => {
-   res.send({data:listOfBirds})
+   res.send({ data: listOfBirds })
 })
 
 app.get('/birds/:id', (req, res) => {
-
-   const matchingId = (bird) => {
-      return bird.id === Number(req.params.id)
-   }
-
+   const matchingId = makeMatchingId(req.params.id)
    const match = listOfBirds.find(matchingId)
 
    if (match !== undefined) {
-      res.send({data: match})
+      res.send({ data: match })
    } else {
       res.sendStatus(404)
    }
-
 })
 
+//post whitout a database to autoincrement the id
+app.post('/birds/', (req, res) => {
+   if (Object.keys(req.body).length === 0) {
+      res.sendStatus(400)
+   } else {
+      // find next available bird ID
+      const ids = listOfBirds.map(bird => bird.id)
+      const nextId = Math.max(...ids) + 1
 
-app.listen(8080, () => {
-   console.log("Server is running on port", 8080)})
+      // put new bird in the list
+      const newBird = req.body
+      newBird['id'] = nextId
+      listOfBirds.push(newBird)
+
+      res.sendStatus(200)
+   }
+})
+
+//update a bird
+app.put('/birds/:id', (req, res) => {
+   const matchingId = makeMatchingId(req.params.id)
+   const match = listOfBirds.find(matchingId)
+
+   if (match !== undefined) {
+      const birdIndex = listOfBirds.findIndex(matchingId)
+      // update entire bird
+      listOfBirds[birdIndex] = req.body
+      // but keep ID of bird, even if req.body contains another ID
+      listOfBirds[birdIndex].id = match.id
+      res.sendStatus(200)
+   } else {
+      res.sendStatus(404)
+   }
+})
+
+// patch a bird
+app.patch('/birds/:id', (req, res) => {
+   const matchingId = makeMatchingId(req.params.id)
+   const match = listOfBirds.find(matchingId)
+
+   if (match !== undefined) {
+      const birdId = match.id
+      const birdIndex = listOfBirds.findIndex(matchingId)
+      // update all keys in JSON body 
+      Object.entries(req.body).forEach(([key, value]) => {
+         listOfBirds[birdIndex][key] = value
+      })
+      // keep ID of bird
+      listOfBirds[birdIndex].id = birdId
+      res.sendStatus(200)
+   } else {
+      res.sendStatus(404)
+   }
+})
+
+//delete whitout a database connection
+app.delete('/birds/:id', (req, res) => {
+   const matchingId = makeMatchingId(req.params.id)
+   const match = listOfBirds.find(matchingId)
+
+   if (match !== undefined) {
+      // make new list of birds, without the bird that is removed
+      listOfBirds = listOfBirds.filter(({ id }) => id !== Number(req.params.id))
+      res.sendStatus(200)
+   } else {
+      res.sendStatus(404)
+   }
+})
